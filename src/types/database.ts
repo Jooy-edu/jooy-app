943f76d851e467462809933b9f1677710bf0d32d
Jooy-edu <jooy.edu@gmail.com>
2025-08-06 11:21:51 +0100

    Updated database.ts

--- src/types/database.ts
+++ src/types/database.ts
@@ -91,7 +91,8 @@ export interface Database {
     }
     Enums: {
       [_ in never]: never
-  role: 'user' | 'admin' | 'student';
+      role: 'user' | 'admin' | 'student';
+    }
     CompositeTypes: {
       [_ in never]: never
     }
